de4b20483c2c2a25417f50a3bc15dd68c5631f43
Terekhova Olga <oterekhova1@gmail.com>
2018-02-07 00:36:01 +0200

    task

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,28 @@ var concatCss = require('gulp-concat-css');
 var connect = require('gulp-connect');
 var  includer = require('gulp-htmlincluder');
 var rename = require('gulp-rename');
+var htmlmin = require('gulp-htmlmin');
 
+
+
+
+gulp.task('server', function(){
+	connect.server({
+		root:'build/',
+		livereload: true
+	});
+});
 
 gulp.task('html', function(){
 	gulp.src('dev/**/*.html')
-		.pipe(includer())
+		.pipe(htmlincluder())
+		.pipe(htmlmin({
+			collapseWhitespace: true
+		}))
 		.pipe(rename(function(path){
 			path.dirname=''
 		}))
 		.pipe(gulp.dest('build/'))
+		.pipe(connect.reload());
 })
-
-
   
