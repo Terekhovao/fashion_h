3efb4af07c1dfc4c4d884ce6d749af7fb3a393b2
Terekhova Olga <oterekhova1@gmail.com>
2018-02-07 23:18:42 +0200

    default task

--- gulpfile.js
+++ gulpfile.js
@@ -20,12 +20,32 @@ gulp.task('html', function(){
 	gulp.src('dev/**/*.html')
 		.pipe(htmlincluder())
 		.pipe(htmlmin({
-			collapseWhitespace: true
+			collapseWhitespace: true,
+			removeComments:1
 		}))
 		.pipe(rename(function(path){
 			path.dirname=''
 		}))
 		.pipe(gulp.dest('build/'))
 		.pipe(connect.reload());
-})
-  
+});
+
+gulp.task('css', function(){
+	gulp.src('dev/assets/css/*.css')
+		.pipe(concat_css())
+		.pipe(csso('css/mystyle.css'))
+		.pipe(gulp.dest('build/'))
+		.pipe(connect.reload());
+});
+
+gulp.task('default', function(){
+	gulp.start(['server', 'html', 'css']);
+
+
+	gulp.watch(['dev/**/*.html'], function(){
+		gulp.start(['html']);
+	});
+	gulp.watch(['dev/assets/css/*.css'], function(){
+		gulp.start(['css']);
+	})
+})  
